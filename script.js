7ebec59b527f8fceeebd47dc9419f6f53b9aef02
noel rodriguez <noelrodriguez@noels-MacBook.fios-router.home>
2021-08-22 19:39:29 -0400

    calculator is working

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ class Calculator {
  
   }
   delete(){
+    this.currentOperand = this.currentOperand.toString().slice(0,-1)
 
   }
   appendNumber(number){
@@ -27,11 +28,55 @@ class Calculator {
    this.currentOperand = ''
   }
   compute(){
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if(isNaN(prev) || isNaN(current))return
+    switch(this.operation) {
+      case '+' :
+        computation =prev + current
+        break
+      case '-' :
+        computation =prev - current
+        break
+      case '*' :
+        computation =prev * current
+        break
+      case '÷' :
+        computation =prev / current
+        break
+        default:
+          return
+
+    }
+     this.currentOperand =computation
+     this.operation = undefined
+     this.previousOperand = " "
 
   }
+  getDisplayNumber(number){
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay 
+    if(isNaN(integerDigits)){
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null){
+      return `${integerDisplay} .${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
+  }
   updateDisplay(){
-    this.currentOperandTextElement.innerText = this.currentOperand
-    this.previousOperandTextElement.innerText = this.previousOperand
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    } else{
+      this.previousOperandTextElement.innerText  =''
+    }
 
 
   }
@@ -42,7 +87,7 @@ class Calculator {
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
-const daleteButton = document.querySelector('[data-delete]')
+const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
@@ -67,4 +112,15 @@ operationButtons.forEach(button => {
 
 equalsButton.addEventListener('click', button =>{
   calculator.compute()
+  calculator.updateDisplay()
+})
+ 
+allClearButton.addEventListener('click', button =>{
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button =>{
+  calculator.delete()
+  calculator.updateDisplay()
 })
